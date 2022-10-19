d8a6568bd783ee0b1b45cac8561e8525302716cb
Nathália <nathaliamarciaandrade@gmail.com>
2022-10-19 15:57:08 -0300

    Requisito 3 concluído

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,12 +1,36 @@
-// import { screen } from '@testing-library/react';
-// import React from 'react';
-// import FavoritePokemons from '../pages/FavoritePokemons';
-// import renderWithRouter from '../renderWithRouter';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-// describe('Verificar a página de Pokémons favoritos', () => {
-//   it('Verificar se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha pokémons favoritos;', () => {
-//     // renderWithRouter(<FavoritePokemons />);
+describe('Verificar a página de Pokémons favoritos', () => {
+  it('Verificar se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha pokémons favoritos', () => {
+    renderWithRouter(<App />);
 
-//     // const =
-// });
-// });
+    const favPok = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favPok).toBeInTheDocument();
+    userEvent.click(favPok);
+    const empty = screen.getByText('No favorite pokemon found');
+    expect(empty).toBeInTheDocument();
+  });
+
+  it('Verificar se são exibidos todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<App />);
+
+    const details = screen.getByRole('link', { name: /more details/i });
+    expect(details).toBeInTheDocument();
+    userEvent.click(details);
+
+    const checkbox = screen.getByRole('checkbox', { name: /Pokémon favoritado?/i });
+    expect(checkbox).toBeInTheDocument();
+    userEvent.click(checkbox);
+
+    const namePok = screen.getByTestId('pokemon-name');
+    expect(namePok).toBeInTheDocument();
+    const typePok = screen.getByTestId('pokemon-type');
+    expect(typePok).toBeInTheDocument();
+    const AveragePok = screen.getByTestId('pokemon-weight');
+    expect(AveragePok).toBeInTheDocument();
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import Pokedex from '../pages/Pokedex';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Verificar a página Pokedex', () => {
+  it('Verificar se a página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<Pokedex />);
+
+    const textH2 = screen.getByRole('heading', { name: /Encountered pokémons/i, level: 2 });
+    expect(textH2).toBeInTheDocument();
+
+    const buttonProx = screen.getByRole('button', { name: 'Próximo pokémon' });
+    expect(buttonProx).toBeInTheDocument();
+    userEvent.click(buttonProx);
+  });
+});
