adb3072905cba1079a992e9556f8f6ce9c991452
Nathália <nathaliamarciaandrade@gmail.com>
2022-10-19 15:16:40 -0300

    Requisito 4 concluído

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+// import { screen } from '@testing-library/react';
+// import React from 'react';
+// import FavoritePokemons from '../pages/FavoritePokemons';
+// import renderWithRouter from '../renderWithRouter';
+
+// describe('Verificar a página de Pokémons favoritos', () => {
+//   it('Verificar se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha pokémons favoritos;', () => {
+//     // renderWithRouter(<FavoritePokemons />);
+
+//     // const =
+// });
+// });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Verificar a página NotFound', () => {
+  it('Verificar se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const textH2 = screen.getByRole('heading', { name: 'Page requested not found', level: 2 });
+    expect(textH2).toBeInTheDocument();
+  });
+
+  it('Verificar se a página contém uma imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    // referência: https://dev.to/raphaelchaula/a-simple-image-test-in-react-3p6f
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute(
+      'src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+    expect(img).toHaveAttribute('alt', 'Pikachu crying because the page requested was not found');
+  });
+});
